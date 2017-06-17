946ccaf2f0cd77912b547b703d4cfad997feb4ef
10110071 <10110071@zte.com.cn>
2017-06-17 23:28:44 +0800

    add test for requirement 3 and make it pass:     3 yard != 4 yard

--- src/YardTest.java
+++ src/YardTest.java
@@ -1,10 +1,21 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class YardTest {
     @Test
     public void should_yard3_equals_yard3() throws Exception {
         assertEquals(new Yard(3), new Yard(3));
+    }
+
+    @Test
+    public void should_yard3_not_equals_yard4() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertNotEquals(new Yard(3), new Yard(4));
     }
 }
