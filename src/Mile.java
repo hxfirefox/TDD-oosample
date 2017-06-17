d0015012a9622a143e681876f20a4326c6ba0349
10110071 <10110071@zte.com.cn>
2017-06-17 23:02:16 +0800

    make test for  3 mile == 3 mile pass

--- src/Mile.java
+++ src/Mile.java
@@ -4,4 +4,15 @@ public class Mile {
     public Mile(int amount) {
         this.amount = amount;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        Mile other = (Mile) o;
+        return this.amount == other.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.amount;
+    }
 }
