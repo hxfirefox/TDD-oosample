266ab1baf9b6e452bdb2ab1a036371ffd09459d0
10110071 <10110071@zte.com.cn>
2017-06-17 23:07:01 +0800

    add test for requirement 1 and make it pass:     3 mile != 2 mile

--- src/MileTest.java
+++ src/MileTest.java
@@ -1,10 +1,21 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class MileTest {
     @Test
     public void should_mile3_equals_mile3() throws Exception {
         assertEquals(new Mile(3), new Mile(3));
+    }
+
+    @Test
+    public void should_mile3_not_equals_mile2() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertNotEquals(new Mile(3), new Mile(2));
     }
 }
