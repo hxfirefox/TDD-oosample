b187b68aa93d6ffacaa3182d6a60f543dbceeeff
10110071 <10110071@zte.com.cn>
2017-06-18 08:27:30 +0800

    refactor test

--- test/LengthTest.java
+++ test/LengthTest.java
@@ -4,12 +4,10 @@ import static org.junit.Assert.*;
 
 public class LengthTest {
     @Test
-    public void should_mile3_equals_length_with_corresponding_amount_and_rate() throws Exception {
+    public void should_equals_length_with_corresponding_amount_and_rate() throws Exception {
         assertEquals(new Mile(3), new Length(3, Length.MILE_RATE));
-    }
-
-    @Test
-    public void should_yard3_equals_length_with_corresponding_amount_and_rate() throws Exception {
         assertEquals(new Yard(3), new Length(3, Length.YARD_RATE));
+        assertEquals(new Feet(3), new Length(3, Length.FEET_RATE));
+        assertEquals(new Inch(3), new Length(3, Length.INCH_RATE));
     }
 }
